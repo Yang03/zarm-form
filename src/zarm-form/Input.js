f01687cdb4909300d97c70ecf74fa2a56ec90d95
yangpan <yangpan@yangpandeMacBook-Pro.local>
2019-09-04 14:29:08 +0800

    1.2.3

--- src/zarm-form/Input.js
+++ src/zarm-form/Input.js
@@ -4,8 +4,8 @@ import { createErrorElement } from './Error'
 
 class Input extends React.PureComponent {
   render() {
-    const { error, hasArrow, label, ...others } = this.props
-    return (<Cell title={label} help={createErrorElement(error)} hasArrow={hasArrow}><ZarmInput {...others} /></Cell>)
+    const { error, hasArrow, label, description, ...others } = this.props
+    return (<Cell title={label} help={createErrorElement(error)} hasArrow={hasArrow} description={description}><ZarmInput {...others} /></Cell>)
   }
 }
 
